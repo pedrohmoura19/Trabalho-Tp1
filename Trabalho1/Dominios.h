e26e58b1f73d2742e47f1a716f8ed26f595cd80e
Roberto Caixeta Ribeiro Oliveira <robertocaixeta102@gmail.com>
2020-09-15 18:20:58 -0300

    :star: Adiciona novas classes no arquivo Dominios.h

--- Trabalho1/Dominios.h
+++ Trabalho1/Dominios.h
@@ -136,6 +136,128 @@ class ValorAplicacao{
         int getValor();
 };
 
+class CodAplicacao{
+    private:
+        static const int FORMATO = 5;
+        string valor;
+        int tamanhoValor = valor.size();
+        void validar(string valor);
+
+    public:
+        CodAplicacao();
+        CodAplicacao(string);
+        void setValor(string);
+        string getValor();
+};
+
+class CodBanco{
+    private:
+        static const int FORMATO = 3;
+        string valor;
+        int tamanhoValor = valor.size();
+        void validar(string valor);
+
+    public:
+        CodBanco();
+        CodBanco(string);
+        void setValor(string);
+        string getValor();
+};
+
+class Cpf{
+    private:
+        static const int TAMANHO = 14;
+        string valor;
+        int tamanhoValor = valor.size();
+        void validar(string valor);
+    public:
+        Cpf();
+        Cpf(string);
+        void setValor(string);
+        string getValor();
+        ~Cpf();
+};
+
+class Emissor{
+    private:
+        static const int LIMITE_MIN = 5;
+        static const int LIMITE_MAX = 30;
+        string valor;
+        void validar(string valor);
+    public:
+        Emissor();
+        Emissor(string);
+        void setValor(string);
+        string getValor();
+        ~Emissor();
+};
+
+class Endereço{
+    private:
+        static const int LIMITE_MIN = 5;
+        static const int LIMITE_MAX = 20;
+        string valor;
+        void validar(string valor);
+    public:
+        Endereço();
+        Endereço(string);
+        void setValor(string);
+        string getValor();
+        ~Endereço();
+};
+
+class Horario{
+    private:
+        string valor;
+        void validar(string valor);
+    public:
+        Horario();
+        Horario(string);
+        void setValor();
+        string getValor();
+        ~Horario();
+};
+
+class Numero{
+    private:
+        static const int TAMANHO = 8;
+        string valor;
+        int tamanhoValor = valor.size();
+        void validar(string valor);
+    public:
+        Numero();
+        Numero(string);
+        void setValor(string);
+        string getValor();
+        ~Numero();
+};
+
+class Senha{
+    private:
+        static const int TAMANHO = 6;
+        int valor;
+        void validar(int valor);
+    public:
+        Senha();
+        Senha(int);
+        void setValor(int);
+        int getValor();
+        ~Senha();
+};
+
+class ValorMinimo{
+    private:
+        int valor;
+        void validar(int valor);
+    public:
+        ValorMinimo();
+        ValorMinimo(int);
+        void setValor(int);
+        int getValor();
+        ~ValorMinimo();
+};
+
+
 
 
 #endif // DOMINIOS_H_INCLUDED
