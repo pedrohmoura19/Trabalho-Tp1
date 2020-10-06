16b23e2f36df759bc469ddf91af961602bc201d4
pedrohmoura19 <pedrohmoura19@gmail.com>
2020-10-05 22:52:37 -0300

    Removed files from folders

--- Trabalho1/doxygen/html/menudata.js
+++ Trabalho1/doxygen/html/menudata.js
@@ -23,16 +23,4 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Classes",url:"annotated.html",children:[
-{text:"Class List",url:"annotated.html"},
-{text:"Class Index",url:"classes.html"},
-{text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html",children:[
-{text:"g",url:"functions.html#index_g"},
-{text:"s",url:"functions.html#index_s"}]},
-{text:"Functions",url:"functions_func.html",children:[
-{text:"g",url:"functions_func.html#index_g"},
-{text:"s",url:"functions_func.html#index_s"}]}]}]},
-{text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"Main Page",url:"index.html"}]}
--- Trabalho1/doxygen/html/navtreedata.js
+++ Trabalho1/doxygen/html/navtreedata.js
@@ -24,24 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "Trabalho1", "index.html", [
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
-    ] ]
-  ] ]
+  [ "Trabalho1", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"_dominios_8h_source.html"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
