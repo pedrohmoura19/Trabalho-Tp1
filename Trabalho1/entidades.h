1ff847c916007b286cf481d1741ed6e53e8872e2
pedrohmoura19 <pedrohmoura19@gmail.com>
2020-10-13 12:49:59 -0300

    Entidades.h implementado
    Novas entidades foram criadas e implementadas

--- Trabalho1/entidades.h
+++ Trabalho1/entidades.h
@@ -1,6 +1,84 @@
 #ifndef ENTIDADES_H_INCLUDED
 #define ENTIDADES_H_INCLUDED
 
+#include "Dominios.h"
+
+
+class Produto{
+ private:
+        CodProduto codigo;
+
+        Classe classe;
+
+        Emissor emissor;
+
+        Prazo prazo;
+
+        Data vencimento;
+
+        Taxa taxa;
+
+        Horario horario;
+
+        ValorMinimo valor;
+
+ public:
+        void setProduto(const CodProduto &codigo);
+        CodProduto getProduto();
+
+        void setClasse(const Classe &classe);
+        Classe getClasse();
+
+        void setEmissor(const Emissor &emissor);
+        Emissor getEmissor();
+
+        void setPrazo(const Prazo &prazo);
+        Prazo getPrazo();
+
+        void setData(const Data &vencimento);
+        Data getData();
+
+        void setTaxa(const Taxa &taxa);
+        Taxa getTaxa();
+
+        void setHorario(const Horario &horario);
+        Horario getHorario();
+
+        void setValorMinimo(const ValorMinimo &valor);
+        ValorMinimo getValorMinimo();
+
+};
+
+
+class Usuario{
+ private:
+        Nome nome;
+
+        Endereco endereco;
+
+        Cep cep;
+
+        Cpf cpf;
+
+        Senha senha;
+
+ public:
+        void setNome(const Nome &nome);
+        Nome getNome();
+
+        void setEndereco(const Endereco &endereco);
+        Endereco getNome();
+
+        void setCep(const Cep &cep);
+        Cep getCep();
+
+        void setCpf(const Cpf &cpf);
+        Cpf getCpf();
+
+        void setSenha(const Senha &senha);
+        Senha getSenha();
+
+};
 
 
 #endif // ENTIDADES_H_INCLUDED
--- Trabalho1/testesEntidades.h
+++ Trabalho1/testesEntidades.h
@@ -1,6 +1,17 @@
 #ifndef TESTESENTIDADES_H_INCLUDED
 #define TESTESENTIDADES_H_INCLUDED
 
+class TUProduto{
+
+
+
+};
+
+class TUUsuario{
+
+
+};
+
 
 
 #endif // TESTESENTIDADES_H_INCLUDED
